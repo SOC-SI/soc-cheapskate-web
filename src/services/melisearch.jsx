ac9b4779550a8d44921832a4df772ed2eb4941b7
timpraferi <93145392+timpraferi@users.noreply.github.com>
2023-06-08 08:22:29 +0200

    Update melisearch.jsx

--- src/services/melisearch.jsx
+++ src/services/melisearch.jsx
@@ -1,7 +1,7 @@
 import { MeiliSearch } from "meilisearch";
 
 export const melisearch = new MeiliSearch({
-  host: "https://ms-02e1a8881220-3785.fra.meilisearch.io",
-  apiKey: "c4721bbf60bf313a2c16e6b0e3e46c954f1a2b303b71d8f424180fab9ed577ef",
+  host: "https://ms-f9b13955e25a-3785.sfo.meilisearch.io",
+  apiKey: "58e8d1bd1fc4c245a2c9e331671d3ff127ff4128a2c2606de11bf0d2481dddfc",
 });
 export default melisearch;
